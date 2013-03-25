61be1cc85af7552cbcd9ccc39eb2cedf25ab922d
Yan.Janus.Le <yan.le@vanceinfo.com.au>
2013-03-25 17:35:24 +1100

    added sample of ssl server

---
+++ ssl/server.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+  "fmt"
+  "crypto/tls"
+  "os"
+  "net"
+  "strconv"
+)
+
+
+func checkError( err error ) {
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(-1)
+  }
+}
+
+
+func try_get_datasize( data *[]byte ) (int, int, error){
+  var i int
+  size := len(*data)
+  for i=0; i<size; i++ {
+    if (*data)[i] == byte(255) {
+      break
+    }
+  }
+
+  if i != size {
+    n, err := strconv.Atoi(string((*data)[0:i]))
+    return n, i, err
+  }
+  return -1, -1, nil
+}
+
+
+func read_data( conn *net.Conn, data *[]byte ) error {
+
+  var (
+    err error
+    n int
+    data_size = -1
+    begin = -1
+    total = 0
+    buf [32]byte
+  )
+
+  // read data
+  for {
+    n, err = (*conn).Read(buf[0:])
+    if err != nil {
+      return err 
+    }
+    *data = append(*data, buf[0:n]...)
+    total += n
+    if data_size == -1 {
+      data_size, begin, err = try_get_datasize(data)
+      if err != nil {
+         return err
+      }
+    }
+    if data_size != -1 && begin != -1 && total-begin-1 == data_size {
+      break
+    }
+  }
+  *data = (*data)[begin+1:]
+  return nil
+}
+
+
+func write_data( conn *net.Conn, data *[]byte ) error{
+  var (
+    size = len(*data)
+    written = 0
+  )
+  for {
+    n, err := (*conn).Write(*data)
+    if err != nil {
+      return err 
+    }
+    written += n
+    if written == size {
+      break
+    }
+  }
+
+  return nil
+}
+
+
+func square( conn *net.Conn ) {
+    data := make([]byte, 0)
+    err := read_data(conn, &data)
+    if err != nil {
+      fmt.Println(err)
+      return
+    }
+    fmt.Println("Calculate squre of " + string(data)) 
+    n, err := strconv.Atoi(string(data))
+    if err != nil {
+      fmt.Println(err)
+      return
+    }
+    n *= n
+    data = []byte(strconv.Itoa(n))
+    write_data(conn, &data) 
+    (*conn).Close()
+}
+
+
+func main() {
+  cert, err := tls.LoadX509KeyPair("zagent.pem", "zagent.pem")
+  checkError(err)
+  config := tls.Config{Certificates: []tls.Certificate{cert}}
+  listener, err := tls.Listen("tcp", ":44443", &config)
+  checkError(err)
+  fmt.Println("Server is started")
+  for {
+    conn, err := listener.Accept() 
+    if err != nil {
+      continue
+    }
+    go square(&conn)
+  }
+}
---
+++ test_script.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# The python script checks the status of the Linux agent
+# Usage: python check_agent.py IP 
+# Author: Yan Le
+
+import socket
+import ssl
+import sys
+import time
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def create_socket(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    while True:
+       try:
+           s.connect( (host, int(port)) )
+       except:
+           s.shutdown(socket.SHUT_RDWR)
+           s.close()
+           time.sleep(1)
+       else:
+           break
+    
+    return ssl.wrap_socket(s, keyfile="sample.pem",
+                              certfile="sample.pem")
+
+
+def normal_command(host, port, code):
+    ssl_sock = create_socket(host, int(port))
+    msg = "33"
+    #ssl_sock.write(chr(code)) #write control
+    ssl_sock.write(str(len(msg)))
+    ssl_sock.write(chr(255)) # end of length
+    ssl_sock.write(msg) #id content
+    #ssl_sock.write('\r\n') # eof mark
+    print ssl_sock.read()
+    s.close()
+  
+if __name__ == '__main__':
+    try:
+
+        import sys
+        if len(sys.argv) <= 1:
+            print "IP doesn't specifiy"
+            sys.exit(-1)
+        ip = sys.argv[1]
+        normal_command(ip, 44443, 0)
+    except Exception, e:
+        sys.stderr.write("Exception: %s" % str(e))
+        sys.exit(-1)
+    sys.exit(0)
+
+
